fd853d6cfcac68ee5f53095cf6d797c143881e23
Suraj07kumar <86525589+Suraj07kumar@users.noreply.github.com>
2023-02-19 01:45:32 +0530

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,36 @@
+function add(){
+    var a = parseInt(document.getElementById('N1').value);
+    var b = parseInt(document.getElementById('N2').value);
+    var c = a + b;
+    console.log(c);
+    document.getElementById('Ans').value = c;
+}
+function sub(){
+    var a = parseInt(document.getElementById('N1').value);
+    var b = parseInt(document.getElementById('N2').value);
+    var c = a - b;
+    console.log(c);
+    document.getElementById('Ans').value = c;
+}
+function mul(){
+    var a = parseInt(document.getElementById('N1').value);
+    var b = parseInt(document.getElementById('N2').value);
+    var c = a * b;
+    console.log(c);
+    document.getElementById('Ans').value = c;
+}
+function div(){
+    var a = parseInt(document.getElementById('N1').value);
+    var b = parseInt(document.getElementById('N2').value);
+    var c = a / b;
+    console.log(c);
+    document.getElementById('Ans').value = c;
+}
+function Root(){
+    var a = parseInt(document.getElementById('N1').value);
+    var b = parseInt(document.getElementById('N2').value);
+    var c = a + b;
+    console.log(c);
+    document.getElementById('Ans').value = c;
+}
+
